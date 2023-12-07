fa6cbd11d92ba9c6493a38f59655f7b25ef432ec
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2023-12-06 18:48:23 -0800

    :sparkles: feat(models/Users.js): create new jwt method on user schema
    created a new createJWT method on UserSchema which generates jwt token. Calls createJWT method within the auth.js controller to generate a new user token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,16 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError } = require('../errors');
-const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
+  const token = user.createJWT();
 
-  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', {
-    expiresIn: '30d',
+  res.status(StatusCodes.CREATED).json({
+    msg: 'register user request successful',
+    user: { name: user.name },
+    token,
   });
-
-  res
-    .status(StatusCodes.CREATED)
-    .json({
-      msg: 'register user request successful',
-      user: { name: user.name },
-      token,
-    });
 };
 
 const login = async (req, res) => {
