7baf364b78b131a9d4047cfcb225d416f1f2c9ba
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2023-12-06 18:26:03 -0800

    feat: :construction: use jwt to genrate token and include token in response
    use the jwt package to generate a user token. Include token and user name in response object.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,22 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError } = require('../errors');
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
 
+  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', {
+    expiresIn: '30d',
+  });
+
   res
     .status(StatusCodes.CREATED)
-    .json({ msg: 'register user request successful', userInfo: user });
+    .json({
+      msg: 'register user request successful',
+      user: { name: user.name },
+      token,
+    });
 };
 
 const login = async (req, res) => {
