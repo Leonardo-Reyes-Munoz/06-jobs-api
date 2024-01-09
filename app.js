917a2b01bd6e0485a7e528223aaca4866035d77b
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2024-01-09 15:46:57 -0800

    feat(public):add javascript and html to handle the front-end of jobs-api

--- app.js
+++ app.js
@@ -40,9 +40,11 @@ app.use(cors());
 app.use(xss());
 
 // testing route for deployment
-app.get('/', (req, res) => {
-  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
-});
+// app.get('/', (req, res) => {
+//   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+// });
+
+app.use(express.static('public'));
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
